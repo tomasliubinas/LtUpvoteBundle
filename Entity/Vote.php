cf49b82303e43099abdac2e84765748c75ef8d6d
Tomas Liubinas <git@liubinas.com>
2017-10-28 01:07:31 +0300

    Add repository and Entity cklass for Vote

---
+++ Entity/Vote.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace LT\UpvoteBundle\Entity;
+
+/**
+ * Vote
+ */
+class Vote
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var int
+     */
+    private $voteAggregateId;
+
+    /**
+     * @var string
+     */
+    private $userId;
+
+    /**
+     * @var string
+     */
+    private $visitorId;
+
+    /**
+     * @var int
+     */
+    private $vote;
+
+    /**
+     * @var \DateTime
+     */
+    private $updatedAt;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set voteAggregateId
+     *
+     * @param integer $voteAggregateId
+     *
+     * @return Vote
+     */
+    public function setVoteAggregateId($voteAggregateId)
+    {
+        $this->voteAggregateId = $voteAggregateId;
+
+        return $this;
+    }
+
+    /**
+     * Get voteAggregateId
+     *
+     * @return int
+     */
+    public function getVoteAggregateId()
+    {
+        return $this->voteAggregateId;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param string $userId
+     *
+     * @return Vote
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return string
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Set visitorId
+     *
+     * @param string $visitorId
+     *
+     * @return Vote
+     */
+    public function setVisitorId($visitorId)
+    {
+        $this->visitorId = $visitorId;
+
+        return $this;
+    }
+
+    /**
+     * Get visitorId
+     *
+     * @return string
+     */
+    public function getVisitorId()
+    {
+        return $this->visitorId;
+    }
+
+    /**
+     * Set vote
+     *
+     * @param integer $vote
+     *
+     * @return Vote
+     */
+    public function setVote($vote)
+    {
+        $this->vote = $vote;
+
+        return $this;
+    }
+
+    /**
+     * Get vote
+     *
+     * @return int
+     */
+    public function getVote()
+    {
+        return $this->vote;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return Vote
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+}
+

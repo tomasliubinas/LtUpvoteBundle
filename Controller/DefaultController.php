8538f27b644189dcebbacd74b1e40d3f1ac5a3da
Tomas Liubinas <git@liubinas.com>
2017-10-31 00:46:32 +0200

    Add Controller

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -3,11 +3,23 @@
 namespace LT\UpvoteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('LTUpvoteBundle:Default:index.html.twig');
+    }
+
+    public function upvoteAction(Request $request)
+    {
+        return new Response('upvoted');
+    }
+
+    public function downvoteAction(Request $request)
+    {
+        return new Response('downoted');
     }
 }

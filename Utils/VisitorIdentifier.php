a11f83b545a9ad63aa26b2a466a92f5b18dbc982
Tomas Liubinas <git@liubinas.com>
2018-01-15 00:37:03 +0200

    identify anonymous visitor

--- Utils/VisitorIdentifier.php
+++ Utils/VisitorIdentifier.php
@@ -6,6 +6,13 @@ use Symfony\Component\HttpFoundation\Request;
 
 class VisitorIdentifier
 {
+    /**
+     * Returns unique anonymous visitor identifier.
+     *
+     * @param Request $request
+     *
+     * @return null|string
+     */
     public function getVisitorId(Request $request)
     {
         return $request->getClientIp();

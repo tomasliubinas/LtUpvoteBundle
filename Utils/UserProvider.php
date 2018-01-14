ec372929fb29d95ac052609c6a4d0623b6aae975
Tomas Liubinas <git@liubinas.com>
2018-01-15 00:31:57 +0200

    Identify authorised user

--- Utils/UserProvider.php
+++ Utils/UserProvider.php
@@ -2,9 +2,9 @@
 
 namespace Lt\UpvoteBundle\Utils;
 
+use Lt\UpvoteBundle\Exception\LtUpvoteBundleException;
 use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
-use Symfony\Component\Security\Core\Role\RoleInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserProvider
@@ -20,48 +20,39 @@ class UserProvider
     }
 
     /**
+     * Returns authorised user
+     *
      * @return UserInterface|null
      */
     public function getUser()
     {
         $token = $this->tokenStorage->getToken();
-
         if ($token !== null) {
-
             if ($token instanceof AnonymousToken) {
                 return null;
             }
-
             return $token->getUser();
         }
-
         return null;
     }
 
     /**
+     * Returns authorised user identifier
+     *
+     * @throws LtUpvoteBundleException
+     *
      * @return int|null
      */
     public function getUserId()
     {
         $user = $this->getUser();
-
         if ($user !== null) {
+            if (!method_exists($user, 'getId')) {
+               throw new LtUpvoteBundleException('User must be identified');
+            }
             return $user->getId();
         }
-
         return null;
     }
 
-    /**
-     * @return null|RoleInterface[]
-     */
-    public function getRoles()
-    {
-        $token = $this->tokenStorage->getToken();
-        if ($token !== null) {
-            return $token->getRoles();
-        }
-
-        return null;
-    }
 }

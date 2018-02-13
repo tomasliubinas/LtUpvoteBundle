00d9e3b06e2e05a7ac564946e236338361371856
Tomas Liubinas <git@liubinas.com>
2018-02-14 01:46:12 +0200

    Add VoteManager to Test Controller

--- Controller/TestController.php
+++ Controller/TestController.php
@@ -2,11 +2,22 @@
 
 namespace Lt\UpvoteBundle\Controller;
 
+use Lt\UpvoteBundle\Model\VoteManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class TestController extends Controller
 {
+    /**
+     * @var VoteManager
+     */
+    private $voteManager;
+
+    public function __construct(VoteManager $voteManager)
+    {
+        $this->voteManager = $voteManager;
+    }
+
     public function indexAction()
     {
         $env = $this->get('kernel')->getEnvironment();
@@ -15,6 +26,6 @@ class TestController extends Controller
             throw new AccessDeniedHttpException('Access denied in production environment');
         }
 
-        return $this->render('LtUpvoteBundle:Default:index.html.twig');
+        return $this->render('LtUpvoteBundle:Default:test.html.twig', ['voteManager' => $this->voteManager]);
     }
 }
